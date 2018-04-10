81f2f58db8b703de17cddb1bade383aa463fea70
aboutmoon <254543199@qq.com>
2018-04-10 15:15:45 +0800

    优化代码结构

--- gridview.js
+++ gridview.js
@@ -1,250 +1,170 @@
-var data = {
-    gridviews: [
-        {
-            tabs: {
-                '1': { //表id
-                    label: 'content1',
-                    fields: 'content1' //该表显示字段，由datagrid控件显示
-                },
-                '2': {
-                    label: 'content2',
-                    fields: 'content2'
-                },
-                '3': {
-                    label: 'content3',
-                    fields: 'content3'
-                },
-                '4': {
-                    label: 'content4',
-                    fields: 'content4'
-                }
-            },
-            weight: 4
-        },
-        {
-            tabs: {
-                '5': {
-                    label: 'content5',
-                    fields: 'content5'
-                },
-                '6': {
-                    label: 'content6',
-                    fields: 'content6'
-                },
-                '7': {
-                    label: 'content7',
-                    fields: 'content7'
-                },
-            },
-            weight: 3
-        },
-        {
-            tabs: {
-                '8': {
-                    label: 'content5',
-                    fields: 'content8'
-                }
-            },
-            weight: 1
-        }
-    ]
-};
-
-var options = {
-    data: data,
-    onBuild: function (fields) { //对应着data.tabs.{key}.fields
-        return '';
-    },
-    onRender: function (obj_pane) { //obj_pane 每个tab下面的content的jquery对象
-        return '';
-    },
-    onGetData: function () {
-        return '';
+;(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('jquery'));
+    } else {
+        root.Dropify = factory(root.jQuery);
     }
-}
+}(this, function($) {
+    var pluginName = "gridview";
 
+    function Gridview(element, options) {
 
+        var defaults = {
+            data: {},
+            onBuild: function (fields) { //对应着data.tabs.{key}.fields
+                return '';
+            },
+            onRender: function (obj_pane) { //obj_pane 每个tab下面的content的jquery对象
 
-;(function ($) {
-
-    //jquery-ui的draggable组件的options
-    var _draggableOptions = {
-
-        revert: 'invalid',
-
-        helper: "clone",
-
-        containment: "document",
-
-        cursor: "move"
-
-    };
-
-    var _droppableOptions = {
-
-        accept: ".tab-pane",
-
-        classes: {
-
-            "ui-droppable-active": "custom-state-active"
-
-        },
-
-        drop: function( event, ui ) {
-
-            if ($(event.target).is(ui.draggable.parent())) {
-                return;
+            },
+            onGetData: function () { //自定义获取数据
+                return '';
             }
-
-            //获取tab和tab对应的content
-            var source_tab_id = ui.draggable.attr('id');
-
-            var $source_tab = ui.draggable
-
-                .parents('.gridview')
-
-                .find(`a[href="#${source_tab_id}"]`)
-
-                .parent('li');
-
-            var $source_content = ui.draggable;
-
-            var $source_tab_container = $source_tab.parents('.nav-tabs');
-
-            var $source_content_container = $source_content.parents('.tab-content');
-
-            //获取容器
-            var $target_tab_container = $(event.target)
-
-                .parent('.gridview')
-
-                .find('.nav-tabs');
-
-            var $target_content_container = $(event.target);
-
-            //将拖动元素和其对应的tab移入容器中。
-            $target_tab_container.append($source_tab);
-
-            $target_content_container.append($source_content);
-
-            //如果gridview下面不存在tab元素，移除gridview
-            if ($source_tab_container.find('li').length == 0) {
-                $source_tab_container.parents('.gridview').remove();
-            } else if ($source_tab_container.find('li').length == 1) {
-                $source_tab_container.hide();
-            } else {
-                $source_tab_container.show();
-            }
-
-            if ($target_tab_container.find('li').length == 0) {
-                $target_tab_container.parents('.gridview').remove();
-            } else if ($target_tab_container.find('li').length == 1) {
-                $target_tab_container.hide();
-            } else {
-                $target_tab_container.show();
-            }
-
-            //如果content 和tab被移除，则第一个tab设置显示
-            $source_tab_container.find('li:first').addClass('active');
-
-            $source_content_container.find('.tab-pane:first').addClass('active in');
-
-            //如果content和tab被移入，则显示移入的tab。
-            $target_tab_container.find('li').removeClass('active');
-
-            $target_content_container.find('.tab-panel').removeClass('active');
-
-            $target_tab_container.find('a:last').tab('show');
-
         }
-    };
+        var _this = this;
+        this._element = element;
+        this._config = $.extend(true, defaults, options);
+        this._draggableOptions = {
+            revert: 'invalid',
+            helper: "clone",
+            containment: "document",
+            cursor: "move"
+        };
+        this._droppableOptions = {
+            accept: ".tab-pane",
+            classes: {
+                "ui-droppable-active": "custom-state-active"
+            },
+            drop: function( event, ui ) {
 
-    var _addDroppableOptions = {
+                if ($(event.target).is(ui.draggable.parent())) {
+                    return;
+                }
+                //获取tab和tab对应的content
+                var source_tab_id = ui.draggable.attr('id');
+                var $source_tab = ui.draggable
+                    .parents('.gridview')
+                    .find(`a[href="#${source_tab_id}"]`)
+                    .parent('li');
+                var $source_content = ui.draggable;
+                var $source_tab_container = $source_tab.parents('.nav-tabs');
+                var $source_content_container = $source_content.parents('.tab-content');
 
-        accept: ".tab-pane",
+                //获取容器
+                var $target_tab_container = $(event.target)
+                    .parent('.gridview')
+                    .find('.nav-tabs');
 
-        classes: {
+                var $target_content_container = $(event.target);
 
-            "ui-droppable-active": "custom-state-active"
+                //将拖动元素和其对应的tab移入容器中。
+                $target_tab_container.append($source_tab);
+                $target_content_container.append($source_content);
 
-        },
+                //如果gridview下面不存在tab元素，移除gridview
+                if ($source_tab_container.find('li').length == 0) {
+                    $source_tab_container.parents('.gridview').remove();
+                } else if ($source_tab_container.find('li').length == 1) {
+                    $source_tab_container.hide();
+                } else {
+                    $source_tab_container.show();
+                }
 
-        drop: function( event, ui ) {
+                if ($target_tab_container.find('li').length == 0) {
+                    $target_tab_container.parents('.gridview').remove();
+                } else if ($target_tab_container.find('li').length == 1) {
+                    $target_tab_container.hide();
+                } else {
+                    $target_tab_container.show();
+                }
 
-            if (ui.draggable.siblings().length <= 1) {
-                return;
+                //如果content 和tab被移除，则第一个tab设置显示
+                $source_tab_container.find('li:first').addClass('active');
+                $source_content_container.find('.tab-pane:first').addClass('active in');
+
+                //如果content和tab被移入，则显示移入的tab。
+                $target_tab_container.find('li').removeClass('active');
+                $target_content_container.find('.tab-panel').removeClass('active');
+                $target_tab_container.find('a:last').tab('show');
+
             }
+        };
+        this._addDroppableOptions =  {
+            accept: ".tab-pane",
+            classes: {
 
-            var gridviewIndex = _this.data('gridview-index');
+                "ui-droppable-active": "custom-state-active"
 
-            var source_tab_id = ui.draggable.attr('id');
+            },
+            drop: function( event, ui ) {
 
-            var $source_tab = ui.draggable
+                if (ui.draggable.siblings().length <= 1) {
+                    return;
+                }
 
-                .parents('.gridview')
+                var $element = $(_this._element);
 
-                .find(`a[href="#${source_tab_id}"]`)
+                var gridviewIndex = $element.data('gridview-index');
+                var source_tab_id = ui.draggable.attr('id');
+                var $source_tab = ui.draggable
+                    .parents('.gridview')
+                    .find(`a[href="#${source_tab_id}"]`)
+                    .parent('li');
 
-                .parent('li');
+                var $source_content = ui.draggable;
+                var $source_tab_container = $source_tab.parents('.nav-tabs');
+                var $source_content_container = $source_content.parents('.tab-content');
 
-            var $source_content = ui.draggable;
-
-            var $source_tab_container = $source_tab.parents('.nav-tabs');
-
-            var $source_content_container = $source_content.parents('.tab-content');
-
-
-            _this.append(`
-
-                <div class="gridview">
-                
-                    <ul id="tab-${gridviewIndex}" class="nav nav-tabs" style="display:none">
-    
-                    </ul>
-                    
-                    <div id="tab-content-${gridviewIndex}" class="tab-content">
-    
+                $element.append(`  
+                    <div class="gridview">                    
+                        <ul id="tab-${gridviewIndex}" class="nav nav-tabs" style="display:none">      
+                        </ul>             
+                        <div id="tab-content-${gridviewIndex}" class="tab-content">
+                        </div>                 
                     </div>
-                    
-                </div>
-            
-            `);
+                `);
 
-            $(`#tab-${gridviewIndex}`).append($source_tab);
+                $(`#tab-${gridviewIndex}`).append($source_tab);
+                $(`#tab-content-${gridviewIndex}`).append($source_content);
+                $(`#tab-content-${gridviewIndex}`).find('.tab-pane').draggable(_this._draggableOptions);
+                $(`#tab-content-${gridviewIndex}`).droppable(_this._droppableOptions);
 
-            $(`#tab-content-${gridviewIndex}`).append($source_content);
+                //如果只有一个tab，则隐藏tab，如果没有tab，将gridview移除，否则显示
+                if ($source_tab_container.find('li').length == 0) {
+                    $source_tab_container.parents('.gridview').remove();
+                } else if ($source_tab_container.find('li').length == 1) {
+                    $source_tab_container.hide();
+                } else {
+                    $source_tab_container.show();
+                }
 
-            $(`#tab-content-${gridviewIndex}`).find('.tab-pane').draggable(_draggableOptions);
+                $source_tab_container.find('li:first').addClass('active');
+                $source_content_container.find('.tab-pane:first').addClass('active in');
+                $element.data('gridview-index', gridviewIndex + 1);
 
-            $(`#tab-content-${gridviewIndex}`).droppable(_droppableOptions);
-
-            //如果只有一个tab，则隐藏tab，如果没有tab，将gridview移除，否则显示
-            if ($source_tab_container.find('li').length == 0) {
-                $source_tab_container.parents('.gridview').remove();
-            } else if ($source_tab_container.find('li').length == 1) {
-                $source_tab_container.hide();
-            } else {
-                $source_tab_container.show();
             }
 
-            $source_tab_container.find('li:first').addClass('active');
+        };
 
-            $source_content_container.find('.tab-pane:first').addClass('active in');
+        this.init();
+    }
 
-            _this.data('gridview-index', gridviewIndex + 1);
+    var _proto = Gridview.prototype;
 
-        }
+    _proto.init = function () {
 
-    };
+        var __this = this;
 
-    function init(options) {
-        var _this = $(this);
+        var $this = $(this._element);
 
-        var settings = $.extend({}, options);
+        var settings = this._config;
 
         var gridviews = settings.data.gridviews;
 
-        return this.each(function () {
+        return $this.each(function () {
+
             for (var i = 0; i < gridviews.length; i++) {
 
                 var tabs_nav = '';
@@ -291,68 +211,60 @@ var options = {
                     tabs_length++;
                 }
 
-                _this.append(`
-        
+                $(this).append(`
                     <div class="gridview">
-                    
                         <ul id="tab-${i}" class="nav nav-tabs "${tabs_length <= 1? 'style="display:none"': ''}>
-            
                             ${tabs_nav}
-        
-                        </ul>
-                        
+                        </ul>    
                         <div id="tab-content-${i}" class="tab-content">
-        
                             ${tabs_content}
-        
                         </div>
-                        
-                    </div>
-                    
+                    </div> 
                 `);
 
                 $(`#tab-content-${i} .tab-pane`).each(function () {
 
-                    if (typeof settings.onBuild == 'function') {
-
-                        content = settings.onBuild($(this));
-
+                    if (typeof settings.onRender == 'function') {
+                        settings.onRender($(this));
                     }
 
                 })
 
             }
             // 记录一下有多少个tab，方便新增tab的时候根据序号命名
-            _this.data('gridview-index', i);
+            $(this).data('gridview-index', i);
 
 
-            _this.find('.tab-pane').draggable(_draggableOptions);
+            $(this).find('.tab-pane').draggable(__this._draggableOptions);
 
             //当gridview 中的content拖动到其他的gridview中，将tab和content移动到其他gridview中
-            _this.find('.tab-content').droppable(_droppableOptions);
+            $(this).find('.tab-content').droppable(__this._droppableOptions);
 
             //增加一个虚线框，当用户将content拖到虚线框之后，生成新的gridview
-            _this.after(`
+            $(this).after(`
             
                 <div class="new-gridview" style="height: 100px; border: 1px dotted grey"></div>
                     
             `);
 
-            _this.next('.new-gridview').droppable(_addDroppableOptions)
+            $(this).next('.new-gridview').droppable(__this._addDroppableOptions)
 
-            _this.sortable();
+            $(this).sortable();
 
         })
-
     }
 
-    function getData() {
+    _proto.getDate = function() {
 
         var data = {};
 
+        var _this = $(this._element);
+
+        var settings = this._config;
+
         data.gridviews = [];
 
-        this.each(function () {
+        _this.each(function () {
 
             _this = $(this);
 
@@ -369,13 +281,13 @@ var options = {
 
                     if (typeof settings.onGetData == 'function') {
 
-                        //content = settings.onGetData($(`${id}`));
+                        content = settings.onGetData($(`${id}`));
 
                     }
 
                     tabs[content_id] = {
 
-                        'fields': '',
+                        'fields': content?content: '',
                         'label': $(this).text()
                     }
 
@@ -387,37 +299,16 @@ var options = {
 
         })
 
-        console.log(data);
-
     }
 
+    $.fn[pluginName] = function(options) {
+        this.each(function() {
+            if (!$.data(this, pluginName)) {
+                $.data(this, pluginName, new Gridview(this, options));
+            }
+        });
 
-    var methods = {
-
-        init: init,
-
-        getData: getData,
-
-    }
-
-
-    $.fn.gridview = function (method) {
-
-        if (methods[method]) {
-
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-
-        } else if (typeof method === 'object' || !method) {
-
-            return methods.init.apply(this, arguments);
-
-        } else {
-
-            $.error('Method' + method + 'does not exist on jQuery.tooltip');
-
-        }
-
-    }
-
-})(jQuery)
-
+        return this;
+    };
+    return Dropify;
+}))
